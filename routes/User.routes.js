4eab7fb5c20d7f0ae95a78da81aefa18600d2c01
Saksham033 <sakshamgoyal02@gmail.com>
2024-06-06 13:00:03 +0530

    define API ENDPOINT for Users

---
+++ routes/User.routes.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const router = express.Router();
+
+// Import your User model
+const User = require("../models/User.model");
+
+// Define your User routes here
+
+// Export the router instance
+module.exports = router;
