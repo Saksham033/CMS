5ee13b20092b5c34148c32c102373033467caaf5
Saksham033 <sakshamgoyal02@gmail.com>
2024-06-06 12:59:51 +0530

    define API ENDPOINT for Policies

---
+++ routes/Policy.routes.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const router = express.Router();
+const PolicyController = require("../controllers/Policy.controller");
+router.post("/", PolicyController.createPolicy);
+router.get("/", PolicyController.getPolicies);
+router.get("/:id", PolicyController.getPolicyById);
+router.put("/:id", PolicyController.updatePolicy);
+router.delete("/:id", PolicyController.deletePolicy);
+module.exports = router;
