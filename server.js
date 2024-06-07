a11a8dbc093a6630ecf497373dfe2deaad841a51
Saksham033 <sakshamgoyal02@gmail.com>
2024-06-07 15:30:25 +0530

    Necessary changes

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const cors = require("cors"); // Import cors middleware
+const app = express();
+const mongoose = require("mongoose");
+const claimRoutes = require("./routes/Claim.routes");
+const policyRoutes = require("./routes/Policy.routes");
+const userRoutes = require("./routes/User.routes");
+
+mongoose.connect("mongodb://localhost/cms", { useNewUrlParser: true, useUnifiedTopology: true });
+
+app.use(express.json());
+app.use(cors({ origin: '*' })); // Allow requests from any origin
+app.use("/api/claims", claimRoutes);
+app.use("/api/policies", policyRoutes);
+app.use("/api/users", userRoutes);
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => console.log(`Server started on port ${port}`));
